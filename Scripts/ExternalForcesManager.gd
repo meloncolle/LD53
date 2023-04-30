27a908500fa9188079f64418d1451b88fa21f9db
Gouhadouken <elijah.cyril.williams@gmail.com>
2023-04-30 16:00:37 -0400

    empty push

--- Scripts/ExternalForcesManager.gd
+++ Scripts/ExternalForcesManager.gd
@@ -1,9 +1,19 @@
 extends CharacterBody3D
 
 @export var tiltForce : Vector3
+var targetTiltForce : Vector3
+@export var acceleration = 1.0
 
 func _physics_process(delta):
 	velocity = tiltForce
 	move_and_slide()
 	pass
 
+func _process(delta):
+	if(targetTiltForce.length() > 0 && tiltForce.length() >= targetTiltForce.length()):
+		targetTiltForce = Vector3.ZERO
+	tiltForce = tiltForce.move_toward(targetTiltForce, acceleration * delta)
+
+
+func _on_tilt_timer_timeout():
+	pass # Replace with function body.
