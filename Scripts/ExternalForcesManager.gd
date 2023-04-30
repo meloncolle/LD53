29e2d5f2e14e179bd39343c50a0d8c1c14cbe5fb
Gouhadouken <elijah.cyril.williams@gmail.com>
2023-04-30 16:43:38 -0400

    made ground moving work

--- Scripts/ExternalForcesManager.gd
+++ Scripts/ExternalForcesManager.gd
@@ -1,19 +1,21 @@
 extends CharacterBody3D
 
+@export var direction : Vector3
+@export var targetSpeed = 5.0
+
 @export var tiltForce : Vector3
 var targetTiltForce : Vector3
 @export var acceleration = 1.0
 
 func _physics_process(delta):
-	velocity = tiltForce
-	move_and_slide()
-	pass
-
-func _process(delta):
 	if(targetTiltForce.length() > 0 && tiltForce.length() >= targetTiltForce.length()):
 		targetTiltForce = Vector3.ZERO
 	tiltForce = tiltForce.move_toward(targetTiltForce, acceleration * delta)
-
+	
+	velocity = tiltForce
+	move_and_slide()
 
 func _on_tilt_timer_timeout():
-	pass # Replace with function body.
+	print_debug("timed out")
+	targetTiltForce = direction * targetSpeed;
+	direction *= -1.0
