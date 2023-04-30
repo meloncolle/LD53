7d7fee02a156352d500339c936b46493344f3949
melonbox\melon <melon@melon.cx>
2023-04-29 21:26:26 -0700

    dialog shit kicking my ass

---
+++ DialogAssets/test_dialog.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+@onready var resource: DialogueResource = load("res://DialogAssets/Dialog/test.dialogue")
+
+const Balloon = preload("res://DialogAssets/custom_balloon/balloon.tscn")
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		var balloon: Node = Balloon.instantiate()
+		get_tree().current_scene.add_child(balloon)
+		balloon.start(resource, "outlet_1")
+		return
