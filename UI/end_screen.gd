e2ab3b858fd7872a155b8b41b5f96eece7640550
melonbox\melon <melon@melon.cx>
2023-06-25 14:22:14 -0700

    Add end screen message with button links to YT/Patreon

--- UI/end_screen.gd
+++ UI/end_screen.gd
@@ -9,3 +9,6 @@ func _input (event: InputEvent):
 	# dont allow pausing if we're on this menu
 	if visible && event.is_action_pressed("ui_cancel"):
 		get_viewport().set_input_as_handled()
+
+func open_link (url: String) -> void:
+	OS.shell_open(url)
